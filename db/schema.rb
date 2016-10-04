5b9f9f5ce7c6de6af8169f81b8ce8fc8b66ab9f0
SerjB <bi4hiji@mail.ru>
2016-10-04 17:07:39 +0300

    .

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,29 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161003144644) do
+ActiveRecord::Schema.define(version: 20161003140410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "dispatchers", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.string   "name"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "dispatchers", ["email"], name: "index_dispatchers_on_email", unique: true, using: :btree
-  add_index "dispatchers", ["reset_password_token"], name: "index_dispatchers_on_reset_password_token", unique: true, using: :btree
 
   create_table "orders", force: :cascade do |t|
     t.integer  "dispatcher_id"
