a4bb3e69277af7d33575ce78d6209e78e0349487
Danchek <denisdotsenko7@gmail.com>
2016-10-05 12:55:40 +0300

    Dispatcher added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :dispatchers
 	resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root :to => "orders#new" 
