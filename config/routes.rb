ae953fa4edb13884fdbc60c4fccea8c39915677b
SerjB <bi4hiji@mail.ru>
2016-10-07 18:37:09 +0300

    generated views for dispatchers and g cancan

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :dispatchers
-  resources :orders
-  
-  root to: 'orders#new'
-  get 'orders/new'
-
-  devise_for :drivers, :path => 'drivers' 
-    # devise_for :users, :controllers => { registrations: 'registrations' }
-    # get '/drivers/sign_out' => 'drivers/sessions#destroy'
-    # get '/drivers/sign_in' => 'drivers/sessions#new'
-  
 
   devise_for :admins, :path => 'admins' 
-    # get '/admins/sign_out' => 'adminssessions#destroy'
+  devise_for :drivers, :path => 'drivers'
+  devise_for :dispatchers, :path => 'dispatchers'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :orders
 
-	resources :orders
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'orders#new'
 
-
+  get 'orders/new'
 end
