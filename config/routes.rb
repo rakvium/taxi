87b797434cda9bf87d5a380e1d19a00e42b38d16
SerjB <bi4hiji@mail.ru>
2016-10-07 14:50:53 +0300

    next try to pass tests :D

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :admins do
-    get '/admins/sign_out' => 'devise/sessions#destroy'
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :dispatchers
